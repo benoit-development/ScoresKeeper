6cd9bdd4b9caf0d33a89c8000d0f02d1a77f16db
Benoît <benoit.development@gmail.com>
2016-11-04 17:13:47 +0100

    Form to add a new player

---
+++ resources/lang/en/player.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Player Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for players details
+    |
+    */
+
+    'players' => 'Player|Players',
+    'name' => 'Name',
+    'new_player' => 'New player',
+
+];
---
+++ resources/lang/fr/player.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Player Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for players details
+    |
+    */
+
+    'players' => 'Joueur|Joueurs',
+    'name' => 'Nom',
+    'new_player' => 'Nouveau joueur',
+
+];
