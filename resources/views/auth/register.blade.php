5c4050994048eda4033b1ac149df7dd27d3eaa91
Benoît <benoit.development@gmail.com>
2016-12-01 17:24:06 +0100

    authentication screens translations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">@lang('auth.register_form')</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+    
+                            <div class="col-md-8 col-md-offset-2">
+								<div class="input-group">
+                                	<span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
+                            		<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus placeholder="@lang('auth.name')">
+                            	</div>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -25,10 +27,11 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            <div class="col-md-8 col-md-offset-2">
+								<div class="input-group">
+                                	<span class="input-group-addon"><i class="fa fa-envelope fa-fw" aria-hidden="true"></i></span>
+                                	<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required placeholder="@lang('auth.email_address')">
+                                </div>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -39,10 +42,11 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <div class="col-md-8 col-md-offset-2">
+								<div class="input-group">
+                                	<span class="input-group-addon"><i class="fa fa-key fa-fw" aria-hidden="true"></i></span>
+                                	<input id="password" type="password" class="form-control" name="password" required placeholder="@lang('auth.password')">
+                            	</div>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -53,10 +57,11 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <div class="col-md-8 col-md-offset-2">
+								<div class="input-group">
+                                	<span class="input-group-addon"><i class="fa fa-key fa-fw" aria-hidden="true"></i></span>
+                                	<input id="password" type="password" class="form-control" name="password_confirmation" required placeholder="@lang('auth.confirm_password')">
+                            	</div>
 
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
@@ -67,9 +72,9 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-2">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    @lang('auth.register')
                                 </button>
                             </div>
                         </div>
